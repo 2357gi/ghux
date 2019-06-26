cbd2fdaa24e571cea4e3aae1ff4648c201ad47a4
2357gi <2357.gi@gmail.com>
2019-06-26 15:43:19 +0900

    :recycle: tabをspaceに

--- ghux.plugin.zsh
+++ ghux.plugin.zsh
@@ -9,7 +9,7 @@ GHUX_WITHOUT_USER_NAME=0
 
 
 function print_usage() {
-	cat << EOL
+    cat << EOL
 You dont have ghq or fzf.
 $ go get github.com/motemen/ghq
 $ brew install fzf
@@ -18,46 +18,47 @@ EOL
 }
 
 function get_project_dir() {
-	local _project_dir=$(ghq list|fzf)
-	ghq_root=$(ghq root)
-	echo $ghq_root/$_project_dir
+    local _project_dir=$(ghq list|fzf)
+    ghq_root=$(ghq root)
+    echo $ghq_root/$_project_dir
 
 }
 
 
 function ghux() {
-	if ! (type ghq &> /dev/null && type fzf &> /dev/null); then
-		print_usage
-		exit 1
-	fi
+    if ! (type ghq &> /dev/null && type fzf &> /dev/null); then
+        print_usage
+        exit 1
+    fi
 
-# 	if [[ ! -z $TMUX ]] ; then
-# 		print_usage
-# 		exit 1
-# 	fi
+#     if [[ ! -z $TMUX ]] ; then
+#         print_usage
+#         exit 1
+#     fi
 
-	project_dir=`get_project_dir`
-	cd $project_dir
+    project_dir=`get_project_dir`
+    cd $project_dir
 
-	
-	if [[ $GHUX_WITHOUT_USER_NAME == 0 ]] ; then
-		project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s/%s", $1,$2}' |rev)
-	else;
-		project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s", $1}' |rev)
-	fi
+    
+    if [[ $GHUX_WITHOUT_USER_NAME == 0 ]] ; then
+        project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s/%s", $1,$2}' |rev)
+    else;
+        project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s", $1}' |rev)
+    fi
 
-	# tmuxに既にfzfで選択したプロジェクトのセッションが存在するかどうか
-	is_session=$(tmux list-sessions | awk -v project_name="$project_name" '{if($1 == project_name":"){print 1}}')
-	
-	
-	if [[ $is_session == 1 ]] ; then
-		if [[ ! -z $TMUX ]] ; then
-			tmux switch-client -t $project_name
-		else;
-			tmux attach-session -t $project_name
-		fi
-	else;
-		tmux new -s $project_name
-	fi
+    # tmuxに既にfzfで選択したプロジェクトのセッションが存在するかどうか
+    is_session=$(tmux list-sessions | awk -v project_name="$project_name" '{if($1 == project_name":"){print 1}}')
+    
+    
+    if [[ $is_session == 1 ]] ; then
+        if [[ ! -z $TMUX ]] ; then
+            tmux switch-client -t $project_name
+        else;
+            tmux attach-session -t $project_name
+        fi
+    else;
+        
+        tmux new -s $project_name
+    fi
 }
 
