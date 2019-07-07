7862a87b527da0021478f3eb4fac6d83b8d8c3da
2357gi <2357.gi@gmail.com>
2019-07-07 16:33:20 +0900

    WIP: とりあえず動いた

--- ghux.plugin.zsh
+++ ghux.plugin.zsh
@@ -17,14 +17,6 @@ or detach tmux session
 EOL
 }
 
-function get_project_dir() {
-    local _project_dir=$(ghq list|fzf)
-    ghq_root=$(ghq root)
-    echo $ghq_root/$_project_dir
-
-}
-
-
 function ghux() {
     if ! (type ghq &> /dev/null && type fzf &> /dev/null); then
         print_usage
@@ -36,32 +28,44 @@ function ghux() {
 #         exit 1
 #     fi
 
-    project_dir=`get_project_dir`
-    cd $project_dir
+    project_dir=$(ghq list|fzf)
+    [[ -z $project_dir ]] && echo "Error: you should choice directry." >&2 && return 1
 
-    
+    ghq_root=$(ghq root)
+    project_dir=$ghq_root/$project_dir
+
+
+    # session名にusernameを含めるかどうか
     if [[ $GHUX_WITHOUT_USER_NAME == 0 ]] ; then
         project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s/%s", $1,$2}' |rev)
     else;
         project_name=$( echo $project_dir |rev | awk -F \/ '{printf "%s", $1}' |rev)
     fi
 
+    echo "---------- dev version ---------- "
+
     # tmuxに既にfzfで選択したプロジェクトのセッションが存在するかどうか
-    is_session=$(tmux list-sessions | awk -v project_name="$project_name" '{if($1 == project_name":"){print 1}}')
+    is_session=$(tmux list-sessions | awk -v project_name="$project_name" '{if($1 == project_name":"){print 0}}')
     
-    
-    if [[ $is_session == 1 ]] ; then
-        if [[ ! -z $TMUX ]] ; then
-            tmux switch-client -t $project_name
-        else;
-            tmux attach-session -t $project_name
-        fi
+    # if you in tmux ssesion
+    [[ -n $TMUX ]] && in_tmux=0 ; echo "in_tmux: $in_tmux"
+
+    if [[ -z $is_session ]]; then
+        echo "create new session within if"
+        (TMUX=; tmux new-session -ds $project_name)
+    fi
+
+    # 存在していなかったらnewsession
+    # echo "create new session outof if"
+    # (TMUX= tmux new -s -d $project_name)
+
+    echo "echo \$TMUX: $TMUX "
+    if [[ -n $in_tmux ]] ; then
+        echo "switch client"
+        tmux switch-client -t $project_name
     else;
-        if [[ ! -z $TMUX ]] ; then
-            tmux ___________ $project_name
-        else;
-            tmux new -s $project_name
-        fi
+        echo "attach session"
+        tmux attach-session -t $project_name
     fi
 }
 
